98af50b099c3385f02ceb777db53892f3df692c2
Nguyen Duc Thuan <ndthuanuet@gmail.com>
2018-04-20 17:37:42 +0700

    init project

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Containers
+import Full from '@/containers/Full'
+
+// Views
+import Dashboard from '@/views/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  linkActiveClass: 'open active',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard',
+      name: 'Home',
+      component: Full,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: null
+        }
+
+      ]
+    }
+  ]
+})
